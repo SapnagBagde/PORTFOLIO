a69bde97f9b76c6c0a56fa1aed620e3103cf6e32
SapnagBagde <sapna2001bagde@gmail.com>
2024-05-07 23:32:01 +0530

    add new components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from "./components/navbar/Navbar"
+import Home from "./components/home/Home"
+import About from "./components/about/About";
+import Skills from "./components/skills/Skills";
+import Project from "./components/project/Project"
+import Contact from "./components/contact/Contact"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Home/>
+      <About/>
+      <Skills/>
+      <Project/>
+      <Contact/>
     </div>
   );
 }
